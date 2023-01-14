9851437d5f491e6542faf0a5c5aa282506d0a0a4
Vishwa Vivek Yadav <112635764+Vishwa9011@users.noreply.github.com>
2023-01-14 15:10:44 +0530

    Crud + operation completed

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -1,37 +1,49 @@
-import { Box, Button, Input } from '@chakra-ui/react'
+import { Box, Button, Input, useToast } from '@chakra-ui/react'
 import React, { useState } from 'react'
-
+import { addDoc, collection } from 'firebase/firestore'
+import { db } from '../Firebase/firebase-config'
 // * Initial State;
 const initialState = { "productName": "", "productBrand": "", "productPrice": 0, "productImg": "", }
 
 const AddProduct = () => {
 
+     const toast = useToast();
      const [product, setProduct] = useState(initialState);
+     console.log('product: ', product);
 
      // * Add new Product into firebase Database;
      const AddProduct = async () => {
+          try {
+               const productRef = collection(db, 'products');
+               await addDoc(productRef, product)
 
+               // * Optional;
+               toast({ title: "Product Added In the database", status: 'success', isClosable: true, position: 'top-right', })
+               setProduct(initialState)
+          } catch (error) {
+               console.log('error: ', error);
+          }
      }
 
      // * to upadate state with product-details
      const HandleChange = (e) => {
-
+          setProduct(prev => ({ ...prev, [e.target.name]: e.target.value }))
      }
 
      return (
           <Box>
                <Box w='60%' m='auto' mt='10' border={'2px'} borderRadius='10px' p='5'>
                     <Box my='4'>
-                         <Input placeholder='product-name' name='product-name' onChange={HandleChange} border='1px' />
+                         <Input placeholder='product-name' value={product.productName} name='productName' onChange={HandleChange} border='1px' />
                     </Box>
                     <Box my='4'>
-                         <Input placeholder='product-brand' name='product-brand' onChange={HandleChange} border='1px' />
+                         <Input placeholder='product-brand' value={product.productBrand} name='productBrand' onChange={HandleChange} border='1px' />
                     </Box>
                     <Box my='4'>
-                         <Input placeholder='product-price' name='product-price' onChange={HandleChange} border='1px' />
+                         <Input placeholder='product-price' value={product.productPrice} name='productPrice' onChange={HandleChange} border='1px' />
                     </Box>
                     <Box my='4'>
-                         <Input placeholder='product-img' name='product-img' onChange={HandleChange} border='1px' />
+                         <Input placeholder='product-img' value={product.productImg} name='productImg' onChange={HandleChange} border='1px' />
                     </Box>
                     <Button onClick={AddProduct} border='1px'>Add Product</Button>
                </Box>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,56 @@
-import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer, } from '@chakra-ui/react';
+import { Table, Thead, Tbody, Tr, Th, Td, TableCaption, TableContainer, Image, } from '@chakra-ui/react';
 import { Box, Button, useToast } from '@chakra-ui/react';
+import { collection, deleteDoc, doc, getDocs, updateDoc } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
+import { db } from '../Firebase/firebase-config';
 
 
 const Home = () => {
      const [products, setProducts] = useState([])
-     // const toast = useToast();
-     // console.table(products);
+     const toast = useToast();
+     console.table(products);
 
+     // todo: update the product
      const UpdateProduct = async (id) => {
+          try {
+               const productRef = doc(db, 'products', id);
+               const temp = window.prompt("Please Re-enter the Product_name");
+               await updateDoc(productRef, { productName: temp })
+               getData();
+
+               // * optional
+               toast({ title: "Product Updated", status: 'success', isClosable: true, position: 'top-right', })
+          } catch (error) {
+               console.log('error: ', error);
+          }
 
      }
-
+     // todo: Delete the Product
      const DeleteProduct = async (id) => {
-
+          try {
+               const productRef = doc(db, 'products', id);
+               await deleteDoc(productRef);
+               getData();
+               // * optional
+               toast({ title: "Product Deleted", status: 'success', isClosable: true, position: 'top-right', })
+          } catch (error) {
+               console.log('error: ', error);
+          }
      }
 
+     // * TO get the data from database
      const getData = async () => {
-          
+          try {
+               const productsRef = collection(db, "products")
+               const res = await getDocs(productsRef);
+
+               // * making an readable array for the data
+               const data = res.docs.map((doc) => ({ ...doc.data(), id: doc.id }))
+
+               setProducts(data) //* storing the data into state
+          } catch (error) {
+               console.log('error: ', error);
+          }
      }
 
      useEffect(() => {
@@ -41,19 +74,21 @@ const Home = () => {
                               </Tr>
                          </Thead>
                          <Tbody>
-                              <Tr>
-                                   <Td>id</Td>
-                                   <Td>productName</Td>
-                                   <Td>productBrand</Td>
-                                   <Td>productPrice</Td>
-                                   <Td>productImg</Td>
-                                   <Td>
-                                        <Button onClick>Update</Button>
-                                   </Td>
-                                   <Td>
-                                        <Button onClick>Delete</Button>
-                                   </Td>
-                              </Tr>
+                              {products.map((product) => (
+                                   <Tr key={product.id}>
+                                        <Td>{product.id}</Td>
+                                        <Td>{product.productName}</Td>
+                                        <Td>{product.productBrand}</Td>
+                                        <Td>{product.productPrice}</Td>
+                                        <Td><Image src={product.productImg} alt='' boxSize={100} /></Td>
+                                        <Td>
+                                             <Button onClick={() => UpdateProduct(product.id)}>Update</Button>
+                                        </Td>
+                                        <Td>
+                                             <Button onClick={() => DeleteProduct(product.id)}>Delete</Button>
+                                        </Td>
+                                   </Tr>
+                              ))}
                          </Tbody>
                     </Table>
                </TableContainer>
